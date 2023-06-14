a7bd36dbc5c0db31a468ea0f07f33bd88af941bd
SiddhiiYadav <136458334+SiddhiiYadav@users.noreply.github.com>
2023-06-14 17:32:05 +0530

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,11 @@
+print("first code")
+a=10
+print("number of code it consists:",a)
+n=input("enter your name")
+print("name of student is:",n)
+m1=int(input("enter your marks"))
+m2=int(input())
+m3=int(input())
+print("marks:",m1,m2,m3)
+avg=(m1+m2+m3)/3
+print("average=",avg)
